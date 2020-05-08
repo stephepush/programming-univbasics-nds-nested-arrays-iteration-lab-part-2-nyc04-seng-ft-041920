3fd00a6c98b4f3ddb3362a9739fe3edb33bb1769
Stephen Peters <7speter@gmail.com>
2020-05-08 13:39:45 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -7,11 +7,11 @@ def find_min_in_nested_arrays(src)
   row_index = 0
   while row_index < src.count do
     element_index = 0
-    min_value = 100
+    min_value = 1000
     inner_results = []
     while element_index < src[row_index].count do
-      if src[row_index][element_index] < min_value
-        min_value = src[row_index][element_index]
+      if src[row_index][element_index] < min_value #if nested item is less than minimum value
+        min_value = src[row_index][element_index] #then min_value is assigned that value
         #binding.pry
       end
       #outer_results.push(min_value)
@@ -20,5 +20,5 @@ def find_min_in_nested_arrays(src)
     outer_results << lowest_value # "<<" is ruby's shortcut for pushing a single array item to an array
     row_index += 1
   end
-  outer_results
+  outer_resultslearn
 end
